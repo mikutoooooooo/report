11d207f230752884704eb774c23e6731802ef326
Mikuto Haga <e215756@ie.u-ryukyu.ac.jp>
2021-12-06 21:39:22 +0900

    2st commit

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,7 +2,7 @@ import jp.ac.uryukyu.ie.e215756.*;
 
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
+        Warrior hero = new Warrior("戦士", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
@@ -11,7 +11,7 @@ public class Main {
         while( hero.isdead() == false && enemy.isdead() == false ){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
-            hero.attack(enemy);
+            hero.attackWithWeponSkill(enemy);
             enemy.attack(hero);
         }
         System.out.println("戦闘終了");
